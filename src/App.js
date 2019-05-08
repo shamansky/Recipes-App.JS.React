aab3f472fa36eac46525e7cb3675fe6ab81bd660
George Ciurescu <shubst3r@gmail.com>
2019-05-08 01:35:26 +0100

    test

--- src/App.js
+++ src/App.js
@@ -12,23 +12,14 @@ class App extends Component {
   getRecipe = async (e) => {
     const recipeName = e.target.elements.recipeName.value;
     e.preventDefault();
-    const api_call = await fetch(`https://cors-anywhere.herokuapp.com/http://food2fork.com/api/search?key=${API_KEY}&q=${recipeName}&count=9`);
+    const api_call = await fetch(`https://www.food2fork.com/api/search?key=${API_KEY}&q=${recipeName}&count=9`);
     // console.log(recipeName);
 
     const data = await api_call.json();
     // console.log(data.recipes[0].source_url);
     this.setState({recipes: data.recipes});
     console.log(this.state.recipes);
-  }
-  // componentDidMount = () => {
-  //   const json = localStorage.getItem("recipes");
-  //   const recipes = JSON.parse(json);
-  //   this.setState({ recipes });
-  // }
-  // componentDidUpdate = () => {
-  //   const recipes = JSON.stringify(this.state.recipes);
-  //   localStorage.setItem("recipes", recipes);
-  // }
+}
   render() {
     return (
       <div className="App">
